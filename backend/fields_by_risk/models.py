57c4c7459d45264c0560c727d0557e0a4bf95036
idanwekhai <kelvinidanwekhai@gmail.com>
2018-11-17 09:09:42 +0100

    added the risk model and fields model

--- backend/fields_by_risk/models.py
+++ backend/fields_by_risk/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from fields.models import Field
+from risks.models import Risk
 # Create your models here.
+
+
+class FieldByRisk(models.Model):
+	"""Defines the fields by risk"""
+	field = models.ForeignKey(Field, null=False, on_delete=models.CASCADE)
+	risk = models.ForeignKey(Risk, null=False, on_delete=models.CASCADE)
+	value = modles.CharField(max_length=120, null=False)
+
+	def __str__(self):
+		return self.value
