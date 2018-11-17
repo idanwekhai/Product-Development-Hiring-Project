7e0e71619b62f686e00d81b3c683e4e8423a4886
idanwekhai <kelvinidanwekhai@gmail.com>
2018-11-17 08:34:56 +0100

    Added models for risktypes and fieldtypes

---
+++ backend/field_types/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class FieldType(models.Model):
+	""""Defines the field types"""
+	name = models.CharField(max_length=120, null=False)
+	description = models.CharField(max_length=260, null=True)
+
+	def __str__(self):
+		return self.name
